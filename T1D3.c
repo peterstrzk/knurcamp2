2503429c33fc365a73e4c46c36fc2b092638a413
Super Student Knur Campa <student@knur.club>
2024-05-23 21:33:40 +0000

    zadanko T1D3

---
+++ T1D3.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdbool.h>
+/*
+ Zaimplementuj ciało funkcji isPrime która zwróci true jeśli podany int §num§ jest liczba pierwszą.
+ Zwróć false w przeciwnym wypadku.
+ */
+
+
+
+bool isPrime(int num) {
+    int i = 2;
+    int n = num;
+    if (n == 0 || n == 1) {
+        return false;
+    }
+
+    // Checking Prime
+    if (n == i)
+        return true;
+
+    // base cases
+    if (n % i == 0) {
+        return false;
+    }
+    i++;
+  
+    //poniższe nie działają przy 99
+//    int temp, sqrt;
+//
+//    sqrt = num / 2;
+//    printf("%d",sqrt);
+//    temp = 0;
+//    while(sqrt != temp)
+//    {
+//        temp = sqrt;
+//        sqrt = (num/temp + temp)/2;
+//
+//
+//    }
+//    printf("P  of ‘%d’ is ‘%d’", num, sqrt);
+//    if (sqrt % 2 != 0) {
+//        return true;
+//    } else {
+//        return false;
+//        };
+//    int liczba, dzielnik, iterator;
+//    liczba = num;
+//    dzielnik = 1;
+//    iterator = 1;
+//    while(dzielnik <= liczba) {
+//
+//        liczba = liczba / dzielnik;
+//        ++dzielnik;
+//       if(dzielnik <= liczba) {
+//           ++iterator;
+//
+//       }
+//       if(iterator == 2) {
+//           return true;
+//       }
+//    }
+//    printf("%d", iterator);
+//
+//    return 0;
+}
+
+
+void selfTest(void) {
+    printf("Self-checking code:\n");
+    printf("Test 1: %s\n", isPrime(2) ? "POPRAWNY" : "NIEPRAWIDŁOWY");
+    printf("Test 2: %s\n", isPrime(11) ? "POPRAWNY" : "NIEPRAWIDŁOWY");
+    printf("Test 3: %s\n", isPrime(99) ?  "NIEPRAWIDŁOWY" : "POPRAWNY");
+    printf("Test 4: %s\n", isPrime(97) ? "POPRAWNY" : "NIEPRAWIDŁOWY");
+}
+
+int main(void) {
+    int num;
+    printf("Podaj liczbe: ");
+    scanf("%d", &num);
+
+    if (isPrime(num)) {
+        printf("%d jest liczba pierwsza.\n", num);
+    } else {
+        printf("%d nie jest liczba pierwsza.\n", num);
+    }
+
+    // Run the self-checking code
+    selfTest();
+
+    return 0;
+}
