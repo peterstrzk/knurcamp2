6b390891776a7137ed03653abfaf584e3fd97e90
Super Student Knur Campa <student@knur.club>
2024-05-23 22:37:38 +0000

    zadanko T1D4

---
+++ T1D4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// T1D4: Dziki i wskaźniki
+// Zaimplementuj funkcję void swap(int *a, int *b) aby podmienić zawartość a na b i b na a.
+// Zadanie bonusowe: Napisz funkcję, która przyjmuje wskaźnik do innej funkcji.
+// Wywołaj z tej funkcji ten wskaźnik.
+// Spraw aby funkcja pod wskaźnikiem wypisała na ekranie "SIGMA".
+
+//dodatkowe
+void sigmaCall(char *str) {
+    printf("%s", str);
+}
+
+void sigmaFankszyn(void) {
+    char* sigmaPtr = "SIGMA";
+    sigmaCall(sigmaPtr);
+}
+
+//glowne zadanie
+void swap(int *a, int *b) {
+    int swapPlace;
+    swapPlace = *a;
+    *a = *b;
+    *b = swapPlace;
+}
+
+
+
+int main(void) {
+    int x = 10;
+    int y = 20;
+    printf("Przed swapem: x = %d, y = %d\n", x, y);
+    swap(&x, &y);
+    printf("Po swapie: x = %d, y = %d\n", x, y);
+    sigmaFankszyn();
+    return 0;
+}
